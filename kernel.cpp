f420879cb3eb2f188073e5a0389fe000ed5ebdf2
Harisai <64903659+EZCLAP03@users.noreply.github.com>
2025-05-21 19:17:19 -0700

    Update kernel.cpp

--- kernel.cpp
+++ kernel.cpp
@@ -2,7 +2,7 @@ extern "C" int main()
 {
     volatile char* vga_buffer = (volatile char*) 0xb8000; 
     int offset = 0; 
-    for(int i = 0; i < 100; ++i)
+    for(int i = 0; i < 1000; ++i)
     {
         vga_buffer[offset] = 'A' + i; 
         vga_buffer[offset + 1] = 0x0F; 
