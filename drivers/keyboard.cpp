0e6c25d8846a725b8ea34341d69e0ea2962e35b2
weeta-code <victordesouza132@gmail.com>
2025-06-14 02:18:19 -0400

    finished keyboard driver, kmalloc next

--- drivers/keyboard.cpp
+++ drivers/keyboard.cpp
@@ -1,12 +1,8 @@
 #include "../util.h"
 
-extern "C"
-{
-    void add_to_cbuff(cbuffer_t*, char, bool);
-    char get_from_cbuffer(cbuffer_t* buf);
-    char rem_from_cbuff(cbuffer_t* buf);
-
-}
+extern "C" void add_to_cbuff(cbuffer_t* buf, char c, bool is_uppercase);
+extern "C" char get_from_cbuffer(cbuffer_t* buf);
+extern "C" void rem_from_cbuff(cbuffer_t* buf);
 
 const char kbdus[128] = // the entire us keyboard, if we want to try support other keyboard layouts have to add those yourself
 {
@@ -106,7 +102,52 @@ void keyboard_handler(struct regs *r) {
     scancode = inb(0x60);
 
     // if the top bit of the byte we read from the keyboard is et, thjat means that a key has been released
+    if (scancode & 0x80) { // use this to
+        // to check if user releases the special key
+        switch(scancode) {
+                case leftShiftReleased: shift_pressed = false; break;
+            case ctrlReleased: ctrl_pressed = false; break;
+        }
+    }
+    else {
+        switch(scancode) {
+                case leftArrowPressed:  add_to_abuff('L'); break;
+                case rightArrowPressed: add_to_abuff('R'); break;
+                case downArrowPressed:  add_to_abuff('D'); break;
+                case upArrowPressed:    add_to_abuff('U'); break;
+                case leftShiftPressed:  shift_pressed = true; break;
+            case ctrlPressed:        ctrl_pressed = true; break;
+                case capsLockedPressed:   caps_lock = !caps_lock; break;
+                case returnPressed:      add_to_kbuff('\n', 0); break;
 
+                case 0x56: add_to_kbuff(shift_pressed ? '>' : '<', 0); break;
+            case 0xE: add_to_kbuff(8,0); break;
+                default:
+            if (ctrl_pressed)
+                add_to_abuff(kbdus[scancode]);
+            else {
+                add_to_kbuff(kbdus[scancode], shift_pressed | caps_lock);
+            }
+        }
+    }
+}
 
+extern "C" void irq_install_handler(int irq, void (*handler));
 
+void kb_install()
+{
+    keybuff = kmalloc(sizeof(cbuffer_t));
+    actionbuff = kmalloc(sizeof(cbuffer_t));
 
+    keybuff -> size = 1000;
+    keybuff -> top = 0;
+    keybuff -> bot = 0;
+    keybuff -> array = kmalloc(keybuff -> size);
+
+    actionbuff -> size = 1000;
+    actionbuff -> top = 0;
+    actionbuff -> bot = 0;
+    actionbuff -> array = kmalloc(actionbuff -> size);
+
+    irq_install_handler(1, keyboard_handler);
+}
